b2c1d2a3008c0d52fadcf008f811a60b1567c017
Spencer Finnell <spencerfinnell@gmail.com>
2018-07-17 16:42:04 -0400

    Don't double enqueue CSS

--- app/theme/assets.php
+++ app/theme/assets.php
@@ -19,10 +19,15 @@ if ( ! defined( 'ABSPATH' ) ) {
  * @since 1.0.0
  */
 function bigbox_child_enqueue_styles() {
-	$version    = bigbox_child_get_theme_version();
-	$stylesheet = bigbox_get_theme_name();
+	$version           = bigbox_child_get_theme_version();
+	$stylesheet_parent = bigbox_get_theme_name();
+	$stylesheet_child  = $stylesheet_parent . '-child';
 
-	// Base and dynamic styles.
-	wp_enqueue_style( $stylesheet . '-child', get_stylesheet_directory_uri() . '/public/css/app.min.css', [ $stylesheet ], $version );
+	wp_dequeue_style( $stylesheet_parent );
+	wp_enqueue_style( $stylesheet_child, get_stylesheet_directory_uri() . '/public/css/app.min.css', [], $version );
+
+	// Remove attachment from parent stylesheet and output with our own.
+	add_filter( 'bigbox_customize_css_inline', '__return_false' );
+	wp_add_inline_style( $stylesheet_child, bigbox_customize_inline_css() );
 }
 add_action( 'wp_enqueue_scripts', 'bigbox_child_enqueue_styles' );
