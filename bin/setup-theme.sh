11b6be59e94bf47910c17cbca0f448fd209c3caa
Spencer Finnell <spencerfinnell@gmail.com>
2018-07-31 11:46:29 -0400

    Use wp-bin

--- bin/setup-theme.sh
+++ bin/setup-theme.sh
@@ -1,35 +1,30 @@
 #!/bin/bash
 
+# Ensure we have access to wp-bin
+git submodule init
+git submodule update --recursive
+
 # Include useful functions
-. "$(dirname "$0")/includes.sh"
+source "$(dirname "$0")/wp-bin/wp-bin.sh"
 
 # Exit if any command fails
 set -e
 
 # Change to the expected directory
-cd "$(dirname "$0")"
-cd ..
+go_to_root
+
+# Make sure there are no changes in the working tree.  Release builds should be
+# traceable to a particular commit and reliably reproducible.
+check_for_clean_cwd
 
 # Do a dry run of the repository reset. Prompting the user for a list of all
 # files that will be removed should prevent them from losing important files!
-status_message "Resetting the repository to pristine condition."
-git clean -xdf --dry-run
+reset_cwd
 
-if ask "$(error_message "About to delete everything above! Is this okay?")" Y; then
-	# Remove ignored files to reset repository to pristine condition. Previous
-	# test ensures that changed files abort the plugin build.
-	status_message "Cleaning working directory..."
-	git clean -xdf
-else
-	error_message "Aborting."
-	exit 0
-fi
-
-status_message "Installing Node modules..."
+# Run the build
+status_message "Installing dependencies..."
 npm install
+composer install --no-dev
 
-status_message "Installing PHP dependencies..."
-composer install
-
-status_message "Building and watching assets..."
-npm run dev
+status_message "Building assets..."
+npm run build
